7fc5281edc830fe5dce36a9cc68a6fdb96bb6726
tiernanraftery <G00418395@atu.ie>
2025-04-05 19:24:44 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from datetime import date
+
+
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
